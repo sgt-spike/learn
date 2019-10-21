158e4aa7217d7f52e3e932160bd7601487b69de3
Brad Chriss <43274807+sgt-spike@users.noreply.github.com>
2019-10-21 17:19:42 -0400

    Add files via upload

---
+++ learn.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+ <html lang="en">
+<head>
+   <title>Learning Front-End Web Design</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+ </head>
+<body>
+	<h1>Learning Web Development</h1>
+
+</body>
+</html> 
